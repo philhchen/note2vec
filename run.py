4ea5ba2edeccfc3d6136e468edecfac1902b21fd
Phil <philchen2000@gmail.com>
2019-03-04 21:48:42 -0800

    Updated model

---
+++ run.py
@@ -0,0 +1,79 @@
+from skipgram import SkipGram
+from utils import Vocab, create_skipgram_dataset
+import torch.nn as nn
+from torch.autograd import Variable
+import torch.optim as optim
+import torch
+import numpy as np
+import pickle
+
+# Hyperparameters
+embed_size = None
+batch_size = 32
+learning_rate = 0.01
+n_epoch = 20
+
+# File handling
+load_prev = True
+data_file = 'data/jsb-chorales-quarter.pkl'
+model_file = 'results/model{}.bin'.format(embed_size)
+embeddings_file = 'results/embeddings{}.tsv'.format(embed_size)
+meta_file = 'results/meta.tsv'
+
+def train_skipgram(vocab, sg_loader):
+	losses = []
+	loss_fn = nn.L1Loss()
+	model = SkipGram(len(vocab), embed_size)
+	print(model)
+
+	if load_prev:
+		try:
+			model.load_state_dict(torch.load(model_file))
+		except:
+			print('Could not load file')
+
+	optimizer = optim.SGD(model.parameters(), lr=learning_rate)
+
+	for epoch in range(n_epoch):
+		total_loss = 0.0
+		for i, sample_batched in enumerate(sg_loader):
+			sample_batched = sample_batched[0]
+			in_w_var = Variable(sample_batched[:,0])
+			ctx_w_var = Variable(sample_batched[:,1])
+			
+			model.zero_grad()
+			log_probs = model(in_w_var, ctx_w_var)
+			loss = loss_fn(log_probs, Variable(sample_batched[:,2].float()))
+
+			loss.backward()
+			optimizer.step()
+
+			total_loss += loss.data
+
+		losses.append(total_loss.item())
+		print('Epoch:', epoch, 'Loss:', total_loss.item())
+		save_params(vocab, model)
+	return model, losses
+
+def save_params(vocab, model):
+	torch.save(model.state_dict(), model_file)
+	embeddings = np.array(model.embeddings.weight.data) if model.simple else np.array(model.embedding_mat.data)
+	with open(embeddings_file, 'w') as f:
+		for i in range(len(embeddings)):
+			for j in range(len(embeddings[0])):
+				f.write('{}\t'.format(embeddings[i,j]))
+			f.write('\n')
+
+	with open(meta_file, 'w') as f:
+		for i in range(len(vocab)):
+			f.write('{}\n'.format(vocab.num2note(vocab.i2w[i])))
+
+def main():
+	with open(data_file, 'rb') as f:
+		data = pickle.load(f)
+	vocab = Vocab(data)
+	sg_loader = create_skipgram_dataset(chorales=data['train'], vocab=vocab, batch_size=32)
+	sg_model, sg_losses = train_skipgram(vocab, sg_loader)
+
+if __name__ == '__main__':
+	main()
---
+++ skipgram.py
@@ -0,0 +1,39 @@
+import torch
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+
+class SkipGram(nn.Module):
+    def __init__(self, vocab_size, embed_size=None, simple=False):
+        super(SkipGram, self).__init__()
+        if embed_size == None:
+            embed_size = vocab_size + 36
+        self.simple = simple
+        self.embeddings = nn.Embedding(vocab_size, embed_size)
+        self.embedding_mat = nn.Parameter(torch.rand(vocab_size, embed_size))
+        self.mask = self.createMask(simple, vocab_size, embed_size)
+    
+    def forward(self, focus, context):
+        if self.simple:
+            embed_focus = self.embeddings(focus)
+            embed_context = self.embeddings(context)
+        else:
+            embed_focus = F.embedding(focus, self.embedding_mat*self.mask)
+            embed_context = F.embedding(context, self.embedding_mat*self.mask)
+
+        embed_focus = embed_focus.view((embed_focus.shape[0], 1, embed_focus.shape[1]))
+        embed_context = embed_context.view((embed_context.shape[0], embed_context.shape[1], 1))
+        scores = torch.bmm(embed_focus, embed_context)
+        log_probs = F.logsigmoid(scores)
+        return log_probs
+
+    def createMask(self, simple, vocab_size, embed_size):
+        if simple:
+            return torch.ones([vocab_size, embed_size])
+
+        mat = torch.zeros([vocab_size, embed_size], dtype=torch.float)
+        for i in range(vocab_size):
+            for n, j in enumerate([0, 12, 19, 24, 28, 31, 34, 36]):
+                if i + j < embed_size:
+                    mat[i, i+j] = 1/(n+1)
+        return mat
